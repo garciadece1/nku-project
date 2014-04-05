93cb4cbe0d3feaf18d7e135299939ac39c05d64f
Emmanuel Garcia <garciadece1@nku.edu>
2014-04-05 00:18:56 -0400

    Add upload and file routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 NkuProject::Application.routes.draw do
-
+  root "upload#index"
+  post "upload/import" => "upload#import"
+  get "upload/import" => "upload#index"
+  resources :file, only: [:show] do
+   
+    get "download"
+   
+  end
 end
