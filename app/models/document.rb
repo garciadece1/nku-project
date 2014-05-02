e78d3ac3f02856ca3a0e7c9de783faec3d451307
Emmanuel Garcia <garciadece1@nku.edu>
2014-05-02 11:27:45 -0400

    Set ruby version to 2.0.0

--- app/models/document.rb
+++ app/models/document.rb
@@ -51,32 +51,4 @@
     end while self.class.exists?(:key => key)
   end
 
-=begin
-  def file_name
-    read_attribute(:id).to_s << "-" << read_attribute(:name)
-  end
-
-  def file_src
-    Rails.configuration.public_file_path + file_name
-  end
-
-  def self.import(file)
-    if file.nil?
-      nil
-    else
-      doc = Document.new(name: file.original_filename, size: file.size, content_type: file.content_type)
-      if doc.save
-        path = File.join(Rails.configuration.public_file_path, doc.file_name)
-        if (File.open(path, "wb") { |f| f.write(file.read) })
-          doc
-        else
-          doc.delete
-          nil
-        end
-      else
-        nil
-      end
-    end
-  end
-=end
 end
